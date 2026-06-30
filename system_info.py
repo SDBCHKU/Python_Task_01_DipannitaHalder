b8f7543bd00f46640a668751d8d3a6370afeb78b
dipannita <162950466+SDBCHKU@users.noreply.github.com>
2026-06-30 21:22:47 +0530

    Create system_info.py

---
+++ system_info.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+# Your details
+name = "Dipannita Halder"
+college = "Siliguri Institute of Technology"
+branch = "BCA"
+internship = "Cyber Security Summer Internship"
+
+# Current date & time
+current_time = datetime.now()
+
+# Output
+print("Name:", name)
+print("College:", college)
+print("Branch:", branch)
+print("Internship:", internship)
+print("Date & Time:", current_time)
