0e2aaf650f6032980ce3561686709eb1a709542a
dipannita <162950466+SDBCHKU@users.noreply.github.com>
2026-06-30 21:23:11 +0530

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,9 @@
+# Taking input from user
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+tech = input("Enter your favorite technology: ")
+
+# Output
+print("\nHello", name + "!")
+print("You are", age, "years old.")
+print("Your favorite technology is", tech + ".")
